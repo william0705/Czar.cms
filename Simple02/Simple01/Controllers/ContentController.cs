39e09699b75441a3925a78561d685cc008754cbe
william0705 <2570694702@qq.com>
2019-01-29 10:06:14 +0800

    提交新的分支代码

---
+++ Simple02/Simple01/Controllers/ContentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using Microsoft.Extensions.Options;
+using Simple01.Models;
+
+namespace Simple01.Controllers
+{
+    public class ContentController : Controller
+    {
+        private Content _content { get; set; }
+
+        public ContentController(IOptions<Content> options)
+        {
+            _content = options.Value;
+        }
+
+        public IActionResult Index()
+        {
+            
+            return View(new ContentViewModel {Contents = new List<Content>(){ _content } });
+        }
+    }
+}
